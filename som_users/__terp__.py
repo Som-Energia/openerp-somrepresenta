fb8885eaf70f679636eadbb246565bdac3e92e57
joana <joana.figueira@somenergia.coop>
2023-10-23 15:32:38 +0200

    🏗️ initial commit
    Co-authored-by: javikalsan <jcalzadosanchez@gmail.com>
    Co-authored-by: Marite Guerrieri <marite.guerrieri@somenergia.coop>

---
+++ som_users/__terp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Representa Business layer",
+    "description": """
+Module for Representa Virtual Office integration
+    """,
+    "version": "0-dev",
+    "author": "Som Energia",
+    "category": "www",
+    "depends":[
+        "base_extended",
+    ],
+    "init_xml": [],
+    "demo_xml": [
+        "demo/res_partner_demo.xml",
+    ],
+    "update_xml":[
+        "security/ir.model.access.csv",
+    ],
+    "active": False,
+    "installable": True
+}
+
