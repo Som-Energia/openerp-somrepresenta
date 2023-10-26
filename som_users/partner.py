c9baccb253ca5d09d2142e7465d74f35040724bd
javikalsan <1070397+javikalsan@users.noreply.github.com>
2023-10-26 11:44:14 +0200

    ✨ Get partner information
    Co-authored-by: joana <joana.figueira@somenergia.coop>

--- som_users/partner.py
+++ som_users/partner.py
@@ -10,10 +10,15 @@ class Partner(osv.osv):
         partner_obj = self.pool.get('res.partner')
         search_params = [('vat','=', cif)]
         partner_id = partner_obj.search(cursor, uid, search_params)
-
         if partner_id:
-            partner = partner_obj.read(cursor, uid, partner_id)   
-            return partner
+            partner = partner_obj.browse(cursor, uid, partner_id)
+            return dict(
+                nif=str(partner[0].vat),
+                name=str(partner[0].name),
+                email=str(partner[0].address[0].email) if partner[0].address else '',
+                roles=dict(
+                    costumer=partner[0].customer)
+            )
         return None
 
 Partner()
--- som_users/tests/test_som_users.py
+++ som_users/tests/test_som_users.py
@@ -20,23 +20,15 @@ class SomUsersTests(testing.OOTestCase):
     def tearDown(self):
         self.txn.stop()
 
-    def test__get_partner__user_not_exists(self):
-        an_invalid_partner_vat = 123
-        result = self.partner.get_partner(self.cursor, self.uid, an_invalid_partner_vat)
-
-        self.assertIsNone(result)
-
-
     def test__get_partner__user_exists(self):
-        partner_id = self.imd.get_object_reference(
-            self.cursor, self.uid, 'base', 'res_partner_asus'
-        )[1]
-        res_partner_asus_vat = 'S2903826B'
+        res_partner_address_soci_vat = '48591264S'
+        result = self.partner.get_partner(self.cursor, self.uid, res_partner_address_soci_vat)
 
-        partner = self.partner.get_partner(self.cursor, self.uid, res_partner_asus_vat)
-        partner_cif = partner[0]['vat']
-
-        expected_partner_cif = self.res_partner.read(
-            self.cursor, self.uid, partner_id,['vat']
-        )['vat']
-        self.assertEqual(expected_partner_cif, partner_cif)
+        expected_result = dict(
+            nif='48591264S',
+            name='Benedetti, Mario',
+            email='test@test.test',
+            roles=dict(
+                costumer=True)
+        )
+        self.assertEqual(expected_result, result)
