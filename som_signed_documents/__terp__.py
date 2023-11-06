9503eebc5273e39aafd15a5444e652b5c0ce3463
joana <joana.figueira@somenergia.coop>
2023-11-06 17:04:41 +0100

    🚧  som_signed_documents
    * Add new model for signed documents registry
    
    Co-authored-by: David García Garzón <david.garcia@somenergia.coop>
    Co-authored-by: Daniel Estanyol <daniel.estanyol@somenergia.coop>

---
+++ som_signed_documents/__terp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Signed documents registry",
+    "description": """
+        Module for registering signed documents
+    """,
+    "version": "0-dev",
+    "author": "Som Energia",
+    "category": "signature",
+    "depends":[
+        "base_extended",
+        
+    ],
+    "init_xml": [],
+    "demo_xml": [
+    ],
+    "update_xml":[
+    ],
+    "active": False,
+    "installable": True
+}
---
+++ som_signed_documents/som_signed_documents.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+from osv import osv, fields
+from tools.translate import _
+
+"""
+
+Document
+- Persona -signa-> RGPD_OV_REPRESENTA
+- Persona -signa-> CONDICIONS_PARTICULAR -> Contracte
+
+Modelo de documento: RGPD_OV_REPRESENTA, CONDICIONS_PARTICULAR
+   Versions de document: type, version, fecha
+
+RegitryDocumentAFirmar: person, document_type_version, signature_date, documentSignature(null)
+
+DocumentoAFirmar: Persona, Modelo, Fecha, EstaFirmado?, Proces -> giscedata.signatura.document
+ProcesoDeFirma: 
+
+"""
+
+
+class SignedDocumentType(osv.osv):
+
+    _name = "signed.document.type"
+    _columns = {
+        'name': fields.char('Name', size=128, required=True),
+        'code': fields.char('Code', size=128, required=True),
+    }
+
+    _sql_constraints = [
+        ('code_uniq', 'unique("code")', _('The name of the document type code must be unique!')),
+    ]
+
+SignedDocumentType()
+
+class SignedDocumentTypeVersion(osv.osv):
+
+    _name = "signed.document.type.version"
+
+    def _get_name(self, cursor, uid, ids, field_name, arg, context=None):
+        result = {}
+        for rec in self.browse(cursor, uid, ids, context):
+            result[rec.id] = "{code} - {version}".format(
+                code=self.type.code,
+                version=self.date,
+            )
+        return result
+
+    _columns = {
+        'name': fields.function(_get_name, method=True, string='Name', type="char", store=True, size=124),
+        'type': fields.many2one('signed.document.type', 'Type', required=True),
+        'date': fields.datetime('Date'),
+    }
+
+    _constraints = [
+        ('type_date_uniq', 'unique("type","date_")', _('Only one version of the document is accepted by date!')),
+    ]
+
+SignedDocumentTypeVersion()
+
+
+class SignedDocument(osv.osv):
+
+    _name = "signed.document"
+        
+    def _get_name(self, cursor, uid, ids, field_name, arg, context=None):
+        result = {}
+        for rec in self.browse(cursor, uid, ids, context):
+            result[rec.id] = "{version} signed by {signer}".format(
+                version=self.document_version.name,
+                signer=self.signer.name,
+            )
+        return result
+
+    _columns = {
+        'name': fields.function(_get_name, method=True, string='Name', type="char", store=True, size=124),
+        'signer': fields.many2one('res.partner', 'Type', required=True),
+        'document_version': fields.many2one('signed.document.type.version', 'Document version', required=True),
+        'signature_date': fields.datetime('Signature date'), # Null means unsigned
+    }
+
+    _sql_constraints = [
+        ('code_uniq', 'unique("code")', _('The name of the document type code must be unique!')),
+    ]
+
+SignedDocument()
