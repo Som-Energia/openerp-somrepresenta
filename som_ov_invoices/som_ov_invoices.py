51cce8ec5bb4ff4bf1e47864e2db0b312bc1abd0
Marite Guerrieri <marite.guerrieri@somenergia.coop>
2023-12-19 10:32:05 +0100

    ✨ Add invoices module
    Co-authored-by: David García Garzón <david.garcia@somenergia.coop>

---
+++ som_ov_invoices/som_ov_invoices.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from osv import osv
+from som_ov_users.decorators import www_entry_point
+from som_ov_users.exceptions import PartnerNotExists
+
+class SomOvInvoices(osv.osv_memory):
+
+    _name = 'som.ov.invoices'
+    
+    CONCEPT_TYPE = {
+        '01': 'market',
+        '02': 'specific_retribution', 
+        '03': 'services',
+    }
+
+    @www_entry_point(
+        expected_exceptions=(
+            PartnerNotExists,
+        )
+    )
+    def get_invoices(self, cursor, uid, vat, context=None):
+        if context is None:
+            context = {}
+
+        users_obj = self.pool.get('som.ov.users')
+        partner = users_obj.get_customer(cursor, uid, vat)
+        invoice_obj = self.pool.get('giscere.facturacio.factura')
+
+        search_params = [
+           ('partner_id','=', partner.id),
+        ]
+
+        invoice_ids = invoice_obj.search(cursor, uid, search_params)
+
+        invoices = invoice_obj.browse(cursor, uid, invoice_ids)
+        return [
+            dict(
+                contract_number=invoice.polissa_id.name,
+                invoice_number=invoice.name,
+                concept=self.CONCEPT_TYPE[invoice.tipo_factura],
+                emission_date=invoice.date_invoice,
+                first_period_date=invoice.data_inici,
+                last_period_date=invoice.data_final,
+                amount=invoice.amount_total,  
+                liquidation=False,           
+            )
+            for invoice in invoices
+        ]
+
+SomOvInvoices()
---
+++ som_ov_invoices/tests/test_som_ov_invoices.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from destral import testing
+from destral.transaction import Transaction
+
+from .. import som_ov_invoices
+
+
+class SomOvInvoicesTests(testing.OOTestCase):
+
+    def setUp(self):
+        self.pool = self.openerp.pool
+        self.imd = self.pool.get('ir.model.data')
+        self.invoice = self.pool.get('som.ov.invoices')
+
+        self.txn = Transaction().start(self.database)
+
+        self.cursor = self.txn.cursor
+        self.uid = self.txn.user
+
+    def tearDown(self):
+        self.txn.stop()
+
+    base_vat = 'ES48591264S'
+
+    def test__get_invoices__base(self):
+        vat = self.base_vat
+
+        result = self.invoice.get_invoices(self.cursor, self.uid, vat)
+
+        expected_result = [
+            dict(
+                contract_number='103',
+                invoice_number='Factura 0',
+                concept='market',
+                emission_date='2022-10-31',
+                first_period_date='2022-10-01',
+                last_period_date='2022-10-31',
+                amount=28.77,
+                liquidation=False,
+            ),
+        ]
+        self.assertEqual(expected_result, result)
